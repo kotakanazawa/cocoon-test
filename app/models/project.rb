d74d452bc1d52c86215b3322aef0bf189e1e91e2
kota kanazawa <kota0818@gmail.com>
2020-06-27 10:16:43 +0900

    add validation to project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  has_many :tasks, inverse_of: :project
+  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
+  validates :name, presence: true
 end
