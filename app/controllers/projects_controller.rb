6fbd671988996c825c7d935fd3af0f5ca4b28ffc
kota kanazawa <kota0818@gmail.com>
2020-06-27 10:19:47 +0900

    add strong parameter

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:name)
+      params.require(:project).permit(:name, tasks_attributes: [:id, :name, :_destroy])
     end
 end
